18e41382cf4839feb7ebb269fffd6396fc96e58b
Enton <stefan.milovic@delsystems.net>
2023-12-05 09:35:26 +0100

    2

--- genTag.sh
+++ genTag.sh
@@ -6,5 +6,7 @@ git rm file*
 echo asd>file$1
 git add *
 git commit -m "$1"
+git tag -d v1.0.$1
+git push origin :refs/tags/v1.0.$1
 git tag -a v1.0.$1 -m "Release version 1.0.$1"
-git push origin --tags
+git push --tags
